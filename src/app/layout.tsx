525c0da1797e8b2123e8c2f35c4fd8232ad9618c
maxinelobato <maxinelobato89@hotmail.com>
2024-04-25 15:55:27 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
+
+const GTM_ID = 'GTM-TZSMQFCF'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,8 +24,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="sr">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className={`${inter.className} scroll-smooth antialiased`}>
-        <main>{children}</main>
+        <main>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+          {children}
+        </main>
       </body>
     </html>
   )
